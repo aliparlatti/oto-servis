f8dd70294fd6ee26d614422d1decf1889c873498
aliparlatti@gmail.com <aliparlatti@gmail.com>
2022-04-15 00:26:20 +0300

    Tema

---
+++ web/themes/custom/alparslan/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.alparslan = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);

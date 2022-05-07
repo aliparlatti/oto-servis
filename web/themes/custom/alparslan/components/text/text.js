e9f396f46a572ac6a09e2b9026bd5022a62c419b
aliparlatti@gmail.com <aliparlatti@gmail.com>
2022-05-07 10:54:42 +0300

    Genel Düzenleme v3

--- web/themes/custom/alparslan/components/text/text.js
+++ web/themes/custom/alparslan/components/text/text.js
@@ -1,23 +1,15 @@
-(function ($, Drupal, drupalSettings) {
+(function($, Drupal, drupalSettings) {
   Drupal.behaviors.moreRead = {
-    attach: function (context, settings) {
-      jQuery(function ($) {
-        $('.more').once('moreRead').click(function (e) {
-          e.preventDefault();
-          $(this).text(function (i, t) {
-            if($('.dot').css('display') === 'block')
-          {
-            $('.dot').addClass('is-disable')
-            
-          }
-          else
-          {
-            $('.dot').removeClass('is-disable')
-          }
-            return t == Drupal.t('Daha Az') ? Drupal.t('Daha Fazla') : Drupal.t('Daha Az');
-          }).prev('.more-cont').slideToggle();
-        });
-      });
-    }
+      attach: function(context, settings) {
+          jQuery(function($) {
+              $('.more').once('moreRead').click(function(e) {
+                  e.preventDefault();
+                  $(this).text(function(i, t) {
+                      return t == Drupal.t('Daha Az') ? Drupal.t('Daha Fazla') : Drupal.t('Daha Az');
+                  }).prev('.more-cont').slideToggle()
+              });
+          });
+      }
   };
-})(jQuery, Drupal, drupalSettings);
+}
+)(jQuery, Drupal, drupalSettings);
